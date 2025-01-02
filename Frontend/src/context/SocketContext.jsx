02339c582170889a37c16774d53c625346ec226b
T.DAMAN <2005839@kiit.ac.in>
2025-01-02 10:29:31 +0530

    Socket url updated

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://mern-chat-app-yt-5018.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
